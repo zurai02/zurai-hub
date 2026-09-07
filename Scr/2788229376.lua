3ef1d045102d813177bdff244aaae5b37d944505
zurai02 <297417184+zurai02@users.noreply.github.com>
2026-09-07 23:32:04 +0500

    Refactor character respawn and effects handling

--- Scr/2788229376.lua
+++ Scr/2788229376.lua
@@ -1,16 +1,20 @@
-local localPlayer = game:GetService('Players').LocalPlayer;
-local localCharacter = localPlayer.Character;
-localCharacter:FindFirstChildOfClass('Humanoid').Health = 0;
-local newCharacter = localPlayer.CharacterAdded:Wait();
-local spoofFolder = Instance.new('Folder');
-spoofFolder.Name = 'FULLY_LOADED_CHAR';
-spoofFolder.Parent = newCharacter;
-newCharacter:WaitForChild('RagdollConstraints'):Destroy();
-local spoofValue = Instance.new('BoolValue', newCharacter);
-spoofValue.Name = 'RagdollConstraints';
-local name = game.Players.LocalPlayer.Name
-local lol =    game.Workspace:WaitForChild(name)
-local money = Instance.new("Folder",game.Players.LocalPlayer.Character);money.Name = "FULLY_LOADED_CHAR"
-lol.Parent = game.Workspace.Players
-game.Players.LocalPlayer.Character:WaitForChild("BodyEffects")
-game.Players.LocalPlayer.Character.BodyEffects.BreakingParts:Destroy()
+local Players = game:GetService("Players")
+local localPlayer = Players.LocalPlayer
+
+localPlayer.Character:FindFirstChildOfClass("Humanoid").Health = 0
+local newCharacter = localPlayer.CharacterAdded:Wait()
+
+local spoofFolder = Instance.new("Folder")
+spoofFolder.Name = "FULLY_LOADED_CHAR"
+spoofFolder.Parent = newCharacter
+
+newCharacter:WaitForChild("RagdollConstraints"):Destroy()
+local spoofValue = Instance.new("BoolValue")
+spoofValue.Name = "RagdollConstraints"
+spoofValue.Parent = newCharacter
+
+local playerModel = game.Workspace:WaitForChild(localPlayer.Name)
+playerModel.Parent = game.Workspace.Players
+
+local bodyEffects = newCharacter:WaitForChild("BodyEffects")
+bodyEffects.BreakingParts:Destroy()
