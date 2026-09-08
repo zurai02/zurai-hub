64df1d988e942f3d54f81702452dfa2fed6ee8a4
zurai02 <297417184+zurai02@users.noreply.github.com>
2026-09-08 16:44:52 +0500

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zurai02/zurai-hub/main/loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zurai02/zurai-hub/main/Scr/loader.lua"))()
