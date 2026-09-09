eda7fa01583141d1367ba63c1e3d3b2b5002a121
zurai02 <297417184+zurai02@users.noreply.github.com>
2026-09-09 13:03:57 +0500

    Create 98868317791094.lua

---
+++ Scr/games/98868317791094.lua
@@ -0,0 +1,343 @@
+--[================================================================[
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣿⣿⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡭⣿⣿⡿⣷⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡜⣷⣿⣿⢿⡸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣼⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢰⢫⣿⣿⢿⣘⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⢆⡫⢾⣿⡿⣌⠳⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡷⣿⣿⣿⣯⢿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣾⣿⣿⣽⣿⣿⣿⣿⣿⣿⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡌⢖⡫⣿⣟⣇⠳⠼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣽⣿⣿⣿⢿⣾⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⢿⣾⣟⣿⣯⣿⣿⣿⣿⣯⢿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣻⣿⣿⣿⣟⡜⠥⣞⢽⣿⢼⡹⡸⢽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣿⣿⢽⣿⣿⣿⣻⣞⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣟⣿⣟⣿⣾⢿⣻⣿⣿⣿⣿⡯⣿⣿⣿⣿⣿⣿⣿⢾⣿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣟⣯⣷⣿⣿⣿⣿⣿⣻⢌⡳⣽⡞⣿⡇⣿⢥⡛⣼⣿⣿⣿⣿⣝⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣟⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣾⣻⢿⣿⣿⢼⣻⢿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣟⣾⢯⣿⢿⣻⣿⣿⣿⣿⣏⣷⣿⣿⣿⣿⣿⣻⢿⣿⡿⣽⣻⣿⣿⣿⣿⣿⣿⣿⢿⣻⣽⣾⣿⣿⡿⣟⣯⣿⣾⣿⣿⣽⠎⡖⣽⣿⡵⣿⠸⣷⣙⡖⣯⢿⣿⣿⣿⣿⣷⣮⣝⣻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⢿⣿⣿⣿⣯⣿⣿⣿⣟⣿⣿⣽⣯⢿⣿⣯⢯⣿⡞⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢾⣿⣽⣟⣾⡿⣯⣟⣿⣿⣿⣿⣿⣳⢞⣿⣿⣿⣿⣽⣿⣻⣿⢿⣽⣻⣿⣿⣿⢿⣻⣵⣾⣿⣻⡿⣽⡿⣾⣟⡿⣯⡷⣯⢿⣿⣻⣜⡱⣫⣿⣷⢻⡇⢿⣷⢺⣥⢻⣿⣿⣿⡿⣟⣿⣿⣿⣿⣶⣭⣛⡿⣿⣿⣿⣿⣿⣿⣯⢿⣿⣿⣿⣽⣾⣿⣿⣯⣿⣿⡷⣏⣿⣿⣯⣏⣿⢿⣻⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡯⣿⣿⢾⡯⣷⢿⣻⣽⢾⣿⣿⡿⣿⢧⣻⡽⣟⣿⣿⣻⣯⢷⣿⣻⢞⡷⣯⣿⣾⢿⣯⡷⣿⢾⣽⣻⣽⣻⢷⣯⣟⣷⣻⣽⣻⣿⣿⢆⡳⢽⣻⣿⣯⣝⠮⣿⣿⡜⣷⣹⣿⣿⣟⣿⢿⣽⣾⣿⢿⣿⣿⣿⣶⢯⣻⢿⣿⣿⣯⣟⣿⣿⣿⣻⣞⣿⣿⣳⣿⣿⣻⣏⣷⣿⣷⣻⢼⡿⣽⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡗⣿⣿⣻⡽⣽⣻⣽⣞⣿⢿⣳⣿⡿⣟⢶⣻⡿⣿⣿⣻⣿⣻⡾⣽⢾⣻⢷⣿⢯⣟⡾⣽⢯⣟⣾⣳⢯⣟⣻⢾⣹⠾⣵⣻⢼⣻⣿⢧⡹⢞⣿⣿⣿⣎⡷⣹⣿⣞⣽⣧⡟⣿⡾⣟⣯⣿⣻⣾⢿⣻⣾⢿⣽⣿⣿⣿⣾⣽⣿⡞⣿⣿⣿⡽⣯⣿⡿⣽⣿⣿⣽⣻⢼⣿⡷⣻⢾⣿⡯⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣹⣿⣿⣹⢿⡷⣏⣷⢿⣾⣿⢿⣹⡿⣏⡾⣱⣿⢿⣿⣹⣷⣿⣹⣏⢿⣹⣾⣹⡿⣾⣹⣏⡿⣾⣇⣿⣹⠾⡉⠏⡷⢿⣱⢏⡷⣏⣿⣇⠿⣉⣿⣿⣿⣿⢷⣹⣿⣿⡾⣿⣹⢷⡿⣏⡿⣾⣹⣏⣿⣿⣹⣿⣏⣿⣷⣿⣏⣿⣿⣹⣏⣿⣷⣿⣏⣿⣿⣹⣿⣿⡾⣿⢾⣿⣿⢹⢷⣿⢿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣻⣿⡿⣽⢾⣝⣻⡼⣟⡾⣽⣟⣯⣿⢧⣝⣳⢯⣿⣿⣽⣷⣻⣽⣞⡯⣷⢫⣿⣟⠷⠛⠈⠉⠀⠉⠀⠉⠈⠙⠋⢹⢣⠟⣾⡱⣏⠿⣞⡽⣣⢿⣿⣿⣿⣿⢾⣯⣿⣿⣟⡟⡎⠑⠉⠘⠁⠉⠈⠑⠋⠛⠳⠿⣯⣷⡿⣾⣿⣿⣷⣹⣿⣟⡷⣯⣿⣯⢿⣿⣿⣽⣻⢞⣽⣿⣏⣯⢿⣻⣽⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢯⣿⣿⡟⣾⡟⣾⣱⢿⣽⢺⣽⢾⣻⢾⣓⡮⣝⣯⣿⣷⣻⣾⣟⡷⣯⡗⡯⢿⠟⠀⠀⠀⠀⠀⠀⠀⠰⣶⣷⣶⠀⠀⠉⡞⣥⢻⣜⡻⣜⡳⣭⢿⣿⣿⣿⣾⣿⣿⣾⢿⡝⠀⢠⠀⠀⠀⠀⠀ ⠀⠀ ⠀⠀ ⣀⣀⡈⠙⠿⣽⣷⣿⣗⢯⣿⡿⣽⣳⣿⣽⢺⣿⡿⣾⡽⣞⣿⣏⣷⢯⢾⣹⣿⡿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⡿⣽⣹⡽⣹⣧⢟⡾⣝⡾⣯⣟⣯⠧⣟⡜⣷⣯⣿⣽⣳⢿⣽⡧⠟⠀⢸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠀⠀⣯⡔⠸⣼⣳⣾⣵⣯⣽⣣⣟⣿⣿⣿⣿⣿⣿⢯⡗⠌⣴⢧⠀⢀⠀⠀⠀⠀  ⠀⠀ ⠀⠛⠿⠃⡦⡀⠈⠟⡾⣽⣻⢾⣟⣷⣹⡿⣎⢿⣿⣻⢷⣻⡜⣿⣏⣿⡞⣯⢞⣿⣿⢿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡷⣿⣿⣻⣳⢿⡳⣿⣿⢫⣿⡜⡿⣵⡻⢾⣙⢮⣓⣟⡾⣿⣼⣟⣯⣷⡞⣅⠀⠼⢤⠠⠄⠀⢀⠀⢀⢀⡀⠀⠀⠀⢸⡟⣥⢻⡼⣷⡿⣿⣻⣿⡿⣿⣟⣯⣿⣾⣿⣿⢯⣞⡱⣬⢻⡄⠀⠈⢠⠀⠀⠀  ⠀⠀⠄⠀⢀⣿⡔⢀⣬⣑⡳⣝⣾⣟⡾⣱⣿⡍⣾⣿⢯⣟⡷⡭⣿⣯⣿⣯⢽⢾⣯⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⢽⣳⢯⣏⣷⣹⣿⡻⣞⡭⢿⣱⡟⣯⢞⣣⢯⣸⢿⣽⣞⣷⣻⣾⣿⡽⢀⠈⠻⢧⣀⣀⡄⣬⢤⣣⢦⢶⠶⣏⣿⣹⣞⣯⣟⣷⣿⣟⣿⣟⣿⣿⣿⢿⣿⡿⣿⣿⣿⣾⣟⣷⣯⣟⣷⡶⣦⣽⢧⣳⣌⣄⣀⣠⠎⣭⣀⢸⣿⣟⣿⣻⡼⣯⣗⣻⢧⡝⣶⣿⢿⡽⣞⡷⣹⣷⣿⣿⡞⣯⡷⣿⣻
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣳⣿⢞⣯⡟⡦⣽⢞⣿⡗⣯⡟⣭⢷⡻⡵⢎⣳⢳⡬⣟⣿⣞⣷⢯⣿⣿⣟⠤⢋⢾⣱⢻⡼⣹⢎⡷⣹⢎⣯⢻⡝⣶⢻⢮⡷⣻⣞⡷⣿⣽⣾⡿⣿⣾⣿⣟⣿⣿⡷⣿⣞⣯⢷⣻⣞⣷⣻⡽⣞⣯⢷⣻⠾⣽⣻⢞⣵⣯⢸⣿⡾⣟⣷⣻⡷⣹⣞⡏⣾⡱⣿⢯⡿⣽⣓⢿⡷⣿⣟⡯⣿⡗⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⢺⣷⢻⣱⣯⢯⣻⡯⣷⢯⡽⣞⡵⢯⣙⡮⢧⣓⢯⣿⡾⣿⣹⣿⣿⣿⡘⠬⣞⡵⣫⢞⡵⢫⢞⣥⢻⣬⢳⡝⣮⢏⣷⢻⡵⣯⢿⣽⣾⣻⣽⣿⣯⣷⣿⣟⣾⣿⣳⣟⡾⣯⢷⣻⡼⣳⡽⣳⢞⡯⣞⡻⢧⣟⡻⣞⣿⢸⣿⣿⣟⣿⣼⡻⡵⣾⣙⠶⣝⡿⣯⣟⡷⣏⣾⣟⣿⣿⢽⣿⣻⡽⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣞⣿⣻⡾⢧⣿⣿⢞⣭⢳⢯⣷⣹⢞⡽⣳⢸⡝⣧⡹⢎⣿⣿⣿⢧⣿⣿⣿⡇⢚⡵⣫⢗⡹⣎⢻⡜⢦⡳⢎⣧⠻⣼⡹⣎⠿⣼⢯⣟⣾⣳⢿⣻⣾⢿⣟⣾⣯⣿⣾⣳⢯⣟⣭⢷⡳⣽⢣⣟⡵⣫⠾⣵⢫⡟⣼⢳⣻⣿⣸⣿⣿⣯⣟⣶⡻⣵⡻⢜⡻⣼⣹⢷⣻⣽⢷⣺⣯⢿⣿⣺⣿⣳⣿⣻
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢾⣽⡧⣟⣻⣿⣿⣞⣮⠟⣞⣧⣛⣮⢷⡱⢮⣽⢲⢭⡓⣯⣷⣿⡧⣿⣿⣿⢧⢙⡾⣱⣋⠷⣜⣣⢞⣣⡝⡞⣴⡛⡶⣝⡞⣯⣳⢟⣾⣳⢿⣻⣯⣟⡃⣿⣯⣷⡿⣿⣽⣻⡼⣏⡾⣵⢫⡿⣼⢳⣭⢻⡜⣧⢻⡵⣫⢷⡯⣿⣿⣿⣯⣿⢶⡻⣼⣛⠼⣓⢧⡝⣿⡽⣯⡷⣯⣿⢻⣿⣽⣿⣳⡷⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢻⣞⣷⢯⣿⣿⣿⣿⢺⣽⡹⣧⢻⡜⣧⠏⣾⢼⣋⢶⡙⢾⣿⣿⡗⣿⣿⣿⣏⠲⣝⢧⡝⣎⡳⣜⠧⣞⡼⣹⠖⣽⢳⡝⣾⣱⢯⣟⡾⣽⣻⣯⣷⣻⠰⣯⣷⡿⣽⣿⣳⢯⢷⣫⠷⣭⢷⣻⡷⣏⡞⣧⢻⡜⣧⢻⡵⣫⣟⣿⣿⣿⣿⣯⢷⣏⡷⣭⢚⡽⢮⡝⣷⢿⣯⡷⣟⣿⣹⢾⣿⣿⣹⣿⣽
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡝⣾⣻⣿⣿⣿⣿⡯⣗⣳⢯⡗⣯⢞⡵⣋⡾⣏⢮⢇⣻⢚⣿⣿⡟⣼⣿⣿⣿⠸⣝⢮⡜⣮⢳⣭⣛⢶⡹⣵⢫⣗⣯⡽⣞⣭⣟⡾⣽⡿⣽⣷⣿⣳⣟⣷⣿⣽⣟⣾⢯⣟⣯⢷⣛⣧⡟⣾⡽⢧⣻⠼⣧⣛⡞⣧⣻⣽⣎⣿⣿⣿⣿⣯⢷⣞⡳⣏⢧⣛⢧⡻⣼⣟⣯⡿⣽⣿⣽⣿⣿⣿⢳⣿⣯
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣽⣳⢟⣷⣻⣿⣿⣿⢾⡽⣹⢺⣇⢯⢺⡵⢩⣗⡟⣮⣙⠦⡏⣿⣿⣏⣿⣿⣿⣿⡙⣮⣓⢾⡱⣟⡶⢯⣳⠿⣼⣻⣼⡞⣷⣻⢾⣽⣻⣽⣟⡿⣷⣻⢿⣻⢿⡾⣟⣾⣻⣟⡾⣽⢾⡽⣶⣻⣳⣻⢻⣼⢻⡶⢯⣝⡾⣵⣻⢸⣿⣿⣿⣿⣯⠿⣼⢱⣏⡶⢏⡾⣱⠽⣾⣿⡿⣽⣿⣿⣾⣿⣭⡿⣞⣷
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣟⣶⣿⢯⡷⣯⢿⣟⡾⢯⢞⣽⣹⢜⡎⣿⡘⢧⣻⣼⢣⢞⡣⢇⣿⣿⣧⢿⣿⣾⣿⣧⢳⡽⣾⣽⣻⣞⡿⣽⣻⢷⣻⡾⣽⣷⣻⣿⢾⣟⣿⡾⡹⠥⠎⠦⣍⠎⡙⢻⣽⢿⣯⣿⢯⡿⣽⣳⢯⣗⣯⣟⡾⣯⣟⢿⣜⢷⣻⠥⣟⣿⣿⣿⣿⣯⢟⡧⢻⣜⢧⢻⡵⣫⠗⣿⣷⣟⣯⣿⣿⣾⣿⢧⣟⣷⢻
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣾⣿⣿⣿⣻⡝⣾⡿⣽⢫⣟⡮⡟⣼⡸⢧⣙⢧⡷⣞⢭⠚⣤⢓⢾⣿⣯⢿⣿⣿⣯⡷⢩⢾⣿⣞⡷⣯⣿⢷⣟⣯⣷⣿⢿⣾⣿⣽⣿⡿⣯⠗⠁⠀⠀⠀ ⠀⠄⠀⠀⢯⢿⣿⣾⢿⣻⣽⢯⣟⣾⣳⢯⣟⡷⣯⣟⢮⣟⡷⢨⣿⣿⣿⣿⣿⣯⡟⡼⣳⢺⣭⠳⣿⣱⢻⡜⣿⣯⣟⣿⣿⣷⣿⢺⣯⣟⢿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡾⣽⣿⣿⣿⣿⣳⣿⢻⡽⢯⣟⡾⣇⣟⢶⣛⠧⣎⡳⡝⣎⢇⡻⣔⢫⢾⣿⣯⣿⣿⣿⣿⣿⡈⢎⢿⣟⡾⣷⣻⢿⣻⣯⣿⣿⡿⣿⣾⣿⢿⡿⠁⠀⠀⠀⠀⠀⠀ ⡀⠀⠀⠘⡘⣿⣽⣿⣿⣻⣟⡿⣾⡽⣿⣽⣻⢗⣯⠳⠞⡱⢺⣿⣿⣿⣿⣿⣳⢟⡰⢧⣛⣧⢻⡽⣧⢳⣍⢿⣽⣯⣿⣿⣯⣿⢻⡾⣽⢾
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣿⣿⣿⣻⣼⢿⡽⣟⣯⣽⣺⢮⡿⡍⢞⡰⢵⡹⡜⢮⡱⢎⡳⣜⣿⡷⣿⣿⣿⣿⣿⡱⢊⡜⢻⡷⣹⡽⣻⣟⣿⣽⣿⣿⢿⣿⣾⣿⡏⡀⣠⣴⣤⡔⣀⡀⣤⣤⣄⠀⣇⢻⣿⣿⣾⢿⣽⣻⢷⣿⣻⠾⣝⠾⡌⢣⠜⣡⣿⣿⣿⣟⣿⣿⡽⣎⠵⣣⠞⣽⡎⣷⢯⡗⣮⢝⣿⣞⣯⣿⣿⣿⣹⢯⣟⡞
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⢷⣻⣿⣿⣿⢿⣯⣷⣿⡽⣯⢿⣻⢾⡵⠯⢏⣵⢺⣍⢾⡱⢧⣛⢶⡹⢮⡱⢎⣿⣟⣿⣿⣿⣟⣿⣇⠣⡜⡡⢟⡰⢏⢿⡾⣽⢾⣿⣾⢿⣟⣿⣾⠁⣿⣿⣿⣟⡾⡱⢟⡷⢊⠿⣷⣯⢾⣿⢷⡿⣯⣟⣯⣟⡾⢭⡛⡌⢣⠜⣡⠎⣥⣿⣿⣯⣿⢯⣿⢞⡬⢳⢥⢛⡲⣝⠮⣿⣽⣚⠮⣽⡾⣟⣿⣿⣯⢟⣟⡾⣝
+⣿⣿⣿⣿⣿⣿⣿⣿⣏⣿⣿⢿⡽⣯⣿⣿⣿⣿⢿⣻⣯⢟⣫⣴⣿⣻⣼⢷⣞⣯⣽⣳⢯⡾⣝⣧⣛⠧⣿⣯⣿⣿⣿⣿⣺⣧⢛⡴⣉⢎⠲⣌⠣⠟⣭⢟⣿⣯⣟⡾⣿⣽⡘⣿⢿⣿⢿⡡⢓⡌⡖⣩⢂⠽⣟⢾⡽⣯⢟⡳⣿⡟⡼⠱⢋⠴⡘⡥⢚⠥⣚⢴⣿⣿⣳⢿⣻⣟⠮⣜⢣⢎⢧⠳⣬⠳⣝⡾⣭⢻⡜⣻⣟⣿⣿⣿⣻⢾⡽⣏
+⣿⣿⣿⣿⣿⣿⣿⣿⣽⣻⣵⣿⣿⣿⣿⣿⡿⣿⣯⣛⣴⡿⣟⣷⣿⣿⣿⣿⣿⣯⣷⣿⣯⣿⣽⡾⣽⣳⣿⣷⣿⣿⣿⡿⣽⣿⡘⢶⡱⢪⡱⢈⡞⢤⡑⢎⢳⣯⢞⡽⣳⣽⡘⣧⣟⡿⢮⣑⠣⣜⣧⠒⣌⢚⡟⡜⡳⣍⠮⣵⢯⠛⡄⢣⡍⠖⣍⠲⣍⠺⡴⣹⣿⡿⣽⢯⡷⣟⠼⣌⠳⣎⢎⡳⣌⠻⣌⠷⣯⣇⠯⡵⣿⣽⣿⣿⣏⣿⢾⣹
+⣿⣿⣿⣿⣿⣿⣿⣟⣾⣟⣳⣿⣿⣿⣿⣿⡿⣏⣶⡿⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣷⣿⡿⣷⣻⣽⣿⣿⣿⣿⣳⣿⢜⣣⢍⡳⣌⠧⡘⢦⡙⢆⠣⢞⠯⡜⢳⢎⡳⢻⣽⣿⣳⢌⢣⠽⣾⡑⢆⣊⠧⣙⠲⣬⢳⣧⡟⡱⣘⢣⡜⢭⡸⡱⢎⡳⣱⢹⣿⣿⢯⣟⡷⣏⠞⣬⢓⣎⢮⡱⢎⡽⢬⣳⢣⣟⣮⡵⣹⢾⣿⣿⣿⣞⣻⡼
+⣿⣿⣿⣿⣿⣟⣷⣿⢿⣼⣿⣿⣿⣿⣿⡿⣱⣿⣻⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣽⣻⣿⣿⣿⡷⣗⣿⢎⡾⣌⠳⣌⡳⢥⠒⡍⢎⡱⢎⠲⣉⠣⢎⡱⡹⣿⣿⣿⣎⡜⡜⢶⣍⠲⣌⡗⣈⠖⡹⢿⠳⡏⢴⠱⣊⡜⢦⡓⡝⢮⡱⢣⢻⣿⣿⣻⠾⣝⡮⣙⢦⢫⡜⣶⡹⣝⣾⣳⣽⣻⣞⣷⡿⣶⡻⣿⣿⣿⡯⣗⣻
+⣿⣿⣿⣿⢿⣿⣿⢫⣿⣿⣿⣿⣿⣿⣳⡿⣯⢷⣿⣻⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⡷⣿⣽⣿⣿⣿⣻⣏⣿⢎⡷⢪⡝⣲⠝⣎⢧⢹⡘⡴⢋⡕⢪⡱⢆⠦⣑⠹⣿⣿⠆⠳⣌⢻⣜⠢⡝⡏⣔⢫⡱⢎⡽⣘⠎⡵⢣⠞⣥⡛⣜⣣⢝⡣⢿⣟⣿⣳⢿⣹⢶⣙⢮⣳⣽⣾⣽⣿⣿⣿⣿⣿⣿⣿⣿⢿⣽⣻⢿⣿⣿⣽⣳
+⣿⣿⣿⣽⣿⡟⣎⣿⣿⣿⣿⣿⣿⣿⣏⡷⣯⣿⣾⢿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⡿⣿⣿⣿⡟⣧⣿⣿⣿⣯⣷⢏⣿⢎⣞⣳⡜⣱⣛⢮⡳⢦⡙⢶⡩⡜⡱⡜⢬⢲⣉⠞⣿⡟⡌⢃⠆⣿⡎⡴⠳⡜⡜⢦⡙⢮⡳⣌⡻⣜⢯⡝⣶⡹⢮⠵⣎⢳⣹⣿⣹⣯⢷⣫⢞⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣳⣯⢿⣿⣿⣗⣯
+⣿⣿⣿⣿⡟⣹⣧⣿⣿⣿⣿⣿⢷⣿⡧⣿⣳⢿⣯⣿⣟⣿⣿⣿⣿⣿⣿⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⡯⣽⣿⣿⣳⣟⡾⣡⣿⢎⡼⣿⢷⢣⢭⡳⣏⣷⣙⣎⢷⣭⡱⢎⡣⢇⡞⠼⣱⣋⡜⢦⣛⡯⢗⣝⣣⠝⣎⡳⣍⡟⣴⢫⢷⣹⢮⣝⡶⣝⢯⡓⣎⠳⣞⣯⡟⣿⣯⢞⡇⣿⣿⣿⣿⣿⣯⣿⣟⣿⣽⣿⣿⣿⣿⢿⣳⣯⣟⣎⣿⣿⣾
+⣿⣿⣿⡽⣼⣿⢲⣿⣿⣿⣿⣿⣿⣿⡗⣯⣟⣯⢿⣾⢿⣻⣿⣾⣿⣻⣿⣽⣿⣿⣿⣿⣿⣟⣿⣿⢿⢧⣿⣿⣳⢟⣾⡃⢷⣟⠆⣿⡽⣏⣞⢦⡓⣝⢺⣗⣮⣛⣾⣳⡭⢞⡱⢮⡹⣱⢧⢺⡱⣎⠿⣜⡞⣬⢻⡜⢧⣿⣝⣮⢟⣞⣧⡟⣮⢳⡝⢎⣱⣎⣿⡾⣽⣻⡽⣿⢮⡽⢺⣿⣿⣿⣿⣿⡷⣿⣟⣿⣾⣿⣿⡿⣟⣿⢾⡽⣾⣹⣿⣿
+⣿⡿⣾⢱⣿⣿⣹⣿⣿⣿⡿⣾⣿⣿⣛⢷⡽⣞⣿⣽⣿⣿⣿⣳⣿⣻⣿⣽⣿⣿⣿⣿⣽⣿⣿⣿⣿⣺⣿⢷⣛⡾⣧⠙⣾⢿⡘⣼⢿⣽⣞⣯⢷⣎⣧⡜⠮⣟⣾⢿⣿⣭⣛⢶⡹⣭⣏⢷⡳⣭⢟⡼⣝⣮⢳⣏⣿⣾⡽⣞⡿⣚⡾⣹⢣⣳⣼⣯⣷⣿⣿⡇⣿⣳⡽⣻⣿⢼⡹⣿⣿⣿⣿⣾⣿⣽⣻⢾⣻⣿⣿⣿⣿⣟⣯⣟⢾⣽⣿⣿
+⣿⣻⢧⣿⣿⡿⣼⣿⣯⣿⣿⣿⣿⡷⡽⣎⣽⣻⢾⣽⣾⣿⣟⣷⣏⢷⣟⣿⣾⢿⣟⣿⣿⣻⣾⢿⣲⣿⢯⢯⣗⡿⢢⢋⣿⢯⡒⣽⣻⣟⣾⣽⢻⣞⣷⣿⣷⣬⣝⣻⢿⣿⣟⣮⢷⡽⣮⣳⡽⢮⣟⡾⣽⣚⣯⣾⣿⣷⢿⣯⠟⣍⣣⣷⣿⣿⣿⣻⣿⣿⣟⡧⢹⡗⣯⢻⣿⣧⢇⣻⣿⣯⣷⣿⣻⢾⣽⣻⣿⢿⣿⣷⣿⣻⣞⡾⣏⢾⣿⣿
+⣿⣏⣾⣿⣿⡟⣷⣿⣟⣯⣿⣿⣿⢳⣽⡳⡜⣯⢿⣽⣷⣿⣿⣿⡜⢮⡽⣾⡽⣟⣯⡿⣞⡿⣽⢯⣿⣏⡟⣾⢺⡍⣇⣻⣟⠮⣱⢺⣷⣛⣾⡝⣯⠾⣽⢯⣿⣿⣿⣷⣯⣜⡻⢯⣟⣾⢷⣳⣟⡿⣾⣽⣳⣟⣾⢿⣳⠟⣫⣴⣻⣿⣿⣿⣟⣿⣿⣷⣿⣿⣻⡝⢦⢻⡽⣣⢿⣿⣏⡖⣻⣿⣽⣷⣻⢟⡾⣽⡿⣿⣿⢿⣾⢷⣯⣳⠏⣿⣿⣿
+⣿⢼⣿⣿⣿⣏⡷⣿⣿⣿⣿⣿⣟⢳⢮⣟⡵⡸⢿⣽⡾⣟⣯⣿⡿⡌⣞⣳⢽⣛⠾⣝⡯⣟⣵⣿⡻⣼⣹⢞⡯⢜⣰⣿⢺⢱⢣⣟⣾⣳⣭⢿⡾⣹⢎⡿⣽⣾⢿⣻⣿⣿⣿⣿⣼⣻⢯⣷⢯⣟⣷⣯⡷⣿⢛⣫⣴⣿⣿⣿⣿⡿⣿⣟⣿⣻⣞⡿⣽⡷⣟⡿⣌⢳⡻⣽⡚⣿⣿⣷⣙⡾⣷⢯⡝⣾⣟⣿⣿⣿⣿⣿⣯⣟⣾⡱⢻⣧⢿⣿
+⣟⣾⣿⣿⣿⣧⣿⣿⢯⣿⣿⣿⣏⡟⣮⡟⡖⣧⠹⣞⣿⣻⣟⣯⣿⡕⡸⢧⡻⣜⢯⡳⣝⣾⣻⠶⣝⣧⣛⢮⠕⣮⣟⢧⣋⠞⣼⣻⣞⡷⣯⢷⣏⣷⣫⣞⣷⣻⢿⣟⣿⢿⣿⣿⢿⣿⣿⣾⣿⣻⣿⣾⣿⣳⣿⣿⣿⣿⣿⡿⣟⣿⣟⣯⣷⢻⣜⡿⣯⢷⣯⢿⡽⣶⣹⢳⡟⡼⣿⣿⣮⣝⡳⣏⠞⣽⣾⣿⣿⣿⡿⣿⣳⣟⡶⣉⣿⡽⣾⣿
+⣿⣿⣯⣿⣿⣧⣿⣾⣿⣿⣿⣿⡧⣟⡼⣏⡗⣯⢳⣘⡳⢯⡿⣽⢾⣽⡐⣯⢳⢎⡳⣳⡽⣯⢗⣻⡝⡶⢩⡞⣾⣻⢎⡳⣌⣻⣞⣷⣻⢾⣽⣻⣞⣷⣻⣞⡷⣯⣟⣾⡽⣯⣟⣿⡿⣯⣿⣾⣿⣿⣷⣿⣿⣿⣿⣿⣿⢯⣿⣿⡿⣿⣻⡽⣞⡿⣾⡽⣯⢷⣯⢷⣻⡵⣯⣳⣝⣳⡹⣿⣿⣮⡕⣎⢺⡽⣿⣿⣿⡿⣿⣿⡽⣞⢱⢾⣹⢿⡽⣿
+⣿⣷⡿⣿⣾⣷⣻⣿⣿⣿⣿⣿⣱⣏⣿⣏⡿⣜⢧⣧⡙⢯⠽⣹⣛⣮⢳⣭⣿⢨⣳⣯⡟⣵⣫⢷⡹⣡⢻⣽⢳⠝⣪⢵⣞⣷⣻⣾⣽⡿⣞⣷⣻⣞⡷⣯⣟⡷⣛⡾⣽⣳⢾⣽⣻⣷⣻⣿⣽⣿⣿⣿⣿⣿⣻⢾⡽⣿⢿⣯⢟⡷⣯⣟⣯⣟⣷⢿⣽⣻⣞⣯⢷⣻⡵⣏⡾⣥⣳⠹⣾⣟⣷⡘⣼⣹⢿⣽⣷⡿⣟⣷⡛⡴⣟⢺⡽⣯⡗⣿
+⣿⣯⣿⣟⣷⣯⢿⣿⣿⣿⣿⣿⢼⡧⣿⢯⡷⣹⢎⣷⣻⡡⣏⢧⡹⣜⠳⡜⢣⣾⡟⣶⢻⡜⣧⠫⣴⠳⡟⣬⢧⣟⣷⣻⣾⣿⣿⣿⣿⢿⣟⣯⣿⣽⣻⢷⣯⢿⡽⣻⡵⢯⣟⣾⣷⣧⣟⣾⣳⣛⣞⣳⢯⡗⣯⢷⣻⣽⣻⣞⣯⣟⡷⣯⣟⣾⣯⢿⡾⣷⣻⣞⣯⢷⣛⡾⣵⢫⡖⣯⢜⣻⣞⣧⢺⡵⣻⣟⡾⣽⣻⠦⣹⠵⣏⡳⣽⢷⡏⣿
+⣿⣯⣷⣿⣻⣽⣻⣿⣿⣿⣿⡿⣾⡽⣿⣻⣝⢧⡻⣞⣷⣳⡙⣦⠳⣜⡹⢆⡿⢧⣟⡞⣧⢛⡴⢫⡵⣻⡼⣯⣟⡾⣷⣿⣳⣿⣿⣿⢿⣿⣿⣿⣽⣷⣿⣻⣞⣯⢿⣵⣫⢟⡾⣜⡷⣻⣿⣿⡿⣝⣮⡝⣮⢿⣽⣻⢗⣯⢶⣻⢾⣽⣻⣽⣯⢿⣾⣟⣿⣟⣷⣯⣟⣯⢿⡽⣞⡯⣽⣚⢮⡒⢿⣾⡱⢣⡟⣾⠹⣧⢇⣯⢱⡻⣇⢽⣚⣯⣏⣿
+⣿⣽⣾⢿⣽⣿⣹⣿⣿⣿⣿⣟⣷⣻⣽⢿⣏⡶⣻⡽⣞⡷⣧⢻⡱⢎⡵⣻⢏⡿⠼⣙⡤⢯⡜⣷⣛⢷⣻⣳⢯⣿⣟⣾⣿⣿⣷⣿⣿⣿⣿⣿⣿⣾⣟⣷⣻⣞⢿⣮⣏⣟⠾⣝⢾⡳⢯⣿⣿⣟⣾⣽⣯⢟⡾⣭⣟⢾⣯⣟⡿⣞⣿⣳⣿⣿⣻⣾⣿⣯⣿⣾⣻⣾⢯⣟⣷⣫⣗⢯⣳⠽⣎⠷⣏⢷⡼⣡⠟⣼⢏⣖⣫⡽⣸⡚⢦⡿⣧⣿
+⣿⣻⣾⣟⣷⢿⣳⣿⣿⣿⣿⣟⡾⣯⢿⣻⣧⢷⣹⡽⣯⣟⡷⣎⡝⢮⡼⠯⣏⠵⣛⣬⢛⣧⣛⡶⢯⣛⡷⣯⢿⣳⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣟⣷⢯⣟⣶⡻⣞⢯⣛⡮⣝⢯⡞⣿⢿⣿⡻⣼⢯⡽⢶⢯⣟⣶⢯⣿⣻⣟⣿⣽⣾⣿⣿⣽⣿⣿⣷⡿⣯⣿⣞⡷⣽⣞⣻⣼⢻⡼⣹⢻⣎⣧⢳⡹⣎⢯⡲⢵⣋⠧⣝⣣⢿⡷⣿
+⣿⢯⣷⣟⣯⡿⣷⣻⣿⣿⣿⣯⣟⣿⣽⣻⣽⢧⣛⣾⣳⢯⣟⡷⣜⣣⣟⢃⡾⣍⢳⠬⣏⢶⡹⣞⢯⣟⡽⣯⣟⣿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣯⣿⣞⡷⣻⢽⣚⣧⢻⡜⣧⢻⡼⣻⢾⣱⢟⣮⠽⣏⡿⣾⡽⣯⣷⡿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣾⣿⣟⣷⣯⣟⡷⣯⢷⣫⣟⢾⣧⢻⣎⠷⣣⢗⡯⣲⠝⣯⢼⡙⡦⢏⣿⣏⣿
+⣿⣟⣷⣯⢿⣽⣷⣻⣿⣿⣿⡷⣯⣿⡼⣷⡿⣯⡜⡷⣯⣛⡾⡙⡍⣾⡽⢸⠲⣍⢮⢳⢎⡷⣫⡽⣳⢞⡽⣷⣻⣾⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣯⣷⢯⣟⣭⢷⣫⢞⣧⢻⡜⣧⡹⣽⢇⣾⡹⢮⢿⣝⣿⣳⣟⡿⣷⣿⣿⣿⣿⣾⣿⣿⣿⣿⣽⣿⣯⣿⣿⣾⣽⣻⡽⣯⣳⢯⣟⣾⢣⢿⣃⢷⡏⣞⡥⣛⢧⡗⣭⡓⣏⣾⣏⣿
+⣿⢾⣳⣯⣿⣞⡷⣯⣿⣿⣿⣟⣽⡿⣽⣳⣿⣻⠼⣹⠓⡭⢰⡱⣘⣷⣻⢸⢏⡜⣎⢧⡻⣜⢧⣻⢵⣻⡽⣯⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣷⢿⣻⢾⣝⡾⣱⢯⡞⣧⢻⣔⡳⣍⢺⡵⣫⢟⡽⣞⣷⣻⣾⢿⣿⣻⣿⣯⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣷⡿⣯⣷⣻⣗⣯⢿⣞⡷⣏⢾⡹⡞⡵⢮⢵⢫⡾⢱⡲⡹⣜⣾⣽⣻
+⣿⢯⣟⣾⣳⣯⢿⣞⣿⣿⣿⣻⣞⡿⣷⢻⣾⢯⠛⡤⢛⠴⣃⠖⣱⣯⢷⡹⢎⡼⢎⡳⣝⢮⣻⡜⣯⢷⣻⢷⣻⣾⣿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣽⣟⣯⣟⣮⢗⡯⣞⡵⣫⢗⢮⠵⡎⣾⣱⢯⢯⣟⡽⣞⣷⡿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣟⣷⣻⢮⣟⡾⢯⣟⣞⠮⣗⠯⡵⣋⢮⢷⢏⢧⢳⡙⣦⣟⣾⢿
+⡿⣯⣟⣾⣳⢯⡿⣼⣿⣯⣿⢷⣫⣿⢯⡟⢩⢖⡹⢌⠳⡸⢄⠏⣼⢯⣯⢳⡹⣜⣫⢵⢫⡞⣵⢫⣯⢟⡽⣯⢿⣳⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣷⣿⣽⡾⣽⢾⣽⣚⢷⡹⣞⡵⣫⢞⡹⢶⣏⢾⣹⠾⣭⢿⣽⡾⣟⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⡿⣞⣯⣟⡾⣽⣻⡽⣾⣙⣧⠻⣜⠭⣞⡯⡜⣎⢧⣙⢦⣿⢾⣿
+⣿⣳⣟⡾⣽⢯⣟⡾⣷⣟⣯⣿⡽⣞⢋⡜⢣⢞⡸⢌⡣⢕⡪⡑⣯⣟⣾⢳⣙⢦⢳⣎⢷⡹⣞⡽⢮⡿⣽⣻⣟⡿⣽⣾⣿⣻⣿⣿⣿⣿⣿⣿⣿⣽⡿⣾⢷⣟⣯⣟⡶⣏⣯⡳⣝⡞⣵⢫⡝⣎⢞⡧⢯⣟⣽⣻⢾⣽⢿⣯⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⡿⣟⣿⣳⣯⣟⣧⢿⡽⣶⣓⣎⢳⡍⣾⡽⣱⢚⡜⢦⡙⣮⢿⣯⡿
+⣷⢿⣾⣹⣏⡿⣾⢷⣏⡿⣏⣷⠏⣰⠏⡸⢇⡾⡰⢏⡸⢎⢱⢱⡿⣾⢷⢹⣎⢹⡶⣎⢷⡹⣎⡹⣷⢏⣷⢿⣾⣹⣿⣹⡿⣿⣿⢿⣹⣿⣿⡿⣾⣿⣹⡿⣿⡾⣷⣏⣹⡏⣶⢿⣈⢿⡸⣇⡾⣈⡏⣾⢹⡾⣾⣹⢿⡾⣿⣹⣾⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣹⣷⢿⣾⣹⢾⣹⢷⣇⡎⢷⡸⣷⢁⡇⣏⡸⢇⡹⣸⡿⣾⠷
+⣯⣟⣾⢳⣯⣻⡽⣎⣿⣻⡽⢣⠞⣥⢫⡕⣣⠖⡝⢮⡱⣋⢮⡱⣻⣭⢿⡌⣟⢦⣛⣬⠳⣝⢮⡳⣽⢺⡽⣞⣳⣟⡾⣽⣟⣿⣽⡿⣿⣯⣷⡿⣟⡷⣿⣽⣳⣟⡷⣽⣣⢟⡭⣗⢫⢎⡷⡹⢎⢷⣚⢧⡟⡾⣵⢫⡾⣽⢷⡿⣽⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣟⣷⡿⣽⣻⢾⡽⣞⣯⣟⡮⡜⢢⡹⠇⡜⡰⢡⢛⡬⢳⣱⢿⣏⢳
+⣷⣻⡞⣟⡾⣵⣻⡽⣞⠷⣡⢏⡞⡴⢣⡞⣥⡛⣼⢣⢳⡍⣶⠱⣟⡾⣻⢤⢻⡮⡕⣎⢳⡝⣮⢳⡭⣗⢯⣽⢳⣯⢿⡽⣾⡽⣞⣿⡽⣷⢯⣟⣯⣟⣳⢯⣷⢻⣞⢧⡟⣮⢗⣮⢛⡞⣶⢹⡹⢆⣛⢮⣝⣳⡭⣷⣻⡽⣯⢿⣟⣾⣿⣿⣾⣿⣽⣿⢿⣿⢿⣻⣷⣿⢯⣿⢯⡿⣽⢯⣟⠾⣽⣞⣳⠇⣍⠳⣔⢣⠘⡔⠣⡜⢨⢇⣿⡿⣌⠧
+⡷⣯⡽⣯⣽⡳⣏⢷⢋⠶⡱⢮⡜⡵⢫⡜⡶⣹⣒⢯⣓⢾⣡⠟⣽⣷⣻⢎⡜⣷⡹⣬⡓⡾⣱⢏⡾⣹⢞⡮⣟⡼⢯⣽⣳⢿⣽⣳⡿⣽⣻⣞⢷⣫⡽⣏⣾⢻⣼⢫⣞⡵⣫⢞⡹⡞⣥⠯⣽⢸⣹⢺⡜⣧⢻⣼⣳⠿⣽⢯⣟⡿⣾⣷⢿⣯⣿⣻⣿⣻⡿⣟⣿⡾⣟⣯⡿⣽⢯⣟⣞⣻⢳⡯⣽⡘⡴⢩⢆⣋⢖⡨⠱⠌⡥⢾⣯⠳⣌⢳
+⣟⡷⣻⣵⡳⣿⣹⠎⡸⣇⣛⢦⡝⣞⣣⢟⡱⢧⣏⠾⣭⠶⣭⢻⡼⣿⣯⡇⢾⡰⣟⣦⢹⡱⢧⣛⡼⢣⡟⣼⣭⢻⡽⣲⢟⣮⢗⣯⢿⣳⢟⣞⣯⢳⣝⢮⡗⣯⢞⡳⣎⢷⡹⢎⡷⣹⡒⣟⠆⣸⢎⡷⣹⢎⡟⡶⣝⣻⣽⣻⣞⣿⣳⢿⣯⢷⣟⡷⣯⣷⡿⣟⣯⣿⣻⢷⣻⣽⣛⡾⣎⠷⣯⢽⠆⣧⠓⡭⢎⡼⢌⡣⢇⠹⣼⢿⡙⢢⢍⠲
+⣯⣽⣳⡳⣟⣵⠫⣰⢇⡯⣜⡞⣼⡱⣎⢯⣝⡳⣎⠿⣜⣻⡼⢧⣳⢻⣿⣽⢪⣵⠸⣧⢧⡹⢖⣣⡝⣧⣛⠶⣭⢳⡝⣧⡻⣜⢯⡞⣧⢟⡾⣹⢎⡷⢮⣳⢻⡜⣯⢳⡝⣮⡝⣪⢵⢣⣻⠼⡱⢌⡻⣴⢫⡞⣽⢳⣝⣳⢮⣗⣻⢾⣽⣻⣞⠿⣾⣽⣳⢯⣟⡿⣽⢾⣽⣫⢷⣏⢷⡻⣼⢻⣜⡏⡞⡴⢫⡜⣣⢎⢧⡹⣌⢳⢫⡟⠨⣅⠪⣱
+⣷⡳⣽⢽⣹⡞⢡⡳⢎⡷⣚⡼⣥⢻⣜⡳⣎⣷⡹⣏⣯⠷⣽⣫⢷⡭⣿⣟⡦⢯⣛⡬⢷⡙⢮⡱⢞⡱⣎⡻⣜⢧⣛⢶⡹⣝⢮⡝⣮⢻⡜⣧⢯⡝⣧⢏⡷⣹⢎⡷⡹⢦⡝⢦⣛⢮⢇⢣⡱⢎⡱⢧⣛⡼⢣⡟⣼⢣⠿⣜⣯⣞⠷⣧⣻⣛⢷⣺⡽⣻⢾⡽⣏⣿⢲⢯⣳⢞⣯⢳⣭⢳⡞⡸⡵⢭⣓⢮⡕⢮⡲⡕⣎⢧⠳⡌⡱⢄⡓⣼
+⣿⡽⣞⢯⣳⠍⣳⢭⣛⡼⣣⠷⣭⢳⡞⣽⢳⣎⣿⣹⢞⣻⣳⡽⣯⢾⡱⣿⡷⣹⣭⢟⣦⢻⡆⢏⡳⣹⡜⡵⣫⠞⣭⠞⣵⢫⡞⡽⢎⡷⣹⢎⡷⣹⢮⡝⢶⣹⠺⣜⡝⣮⢹⢧⡏⢏⡜⢦⢣⢏⡜⢳⢧⣚⠧⣝⠮⣝⡿⣹⠶⣭⢟⣞⡳⢯⣛⣶⢻⡭⢷⣫⢗⡞⣯⢳⡝⡾⣜⡳⢮⠟⣴⢯⡝⣧⢏⡶⣹⢣⡗⣹⠜⣎⣳⢱⡂⠎⢴⣻
+⠿⣿⡭⣷⢫⣘⢧⣏⢞⡶⣭⢻⣜⢷⣫⢯⡷⣛⡶⣯⣟⡷⣯⣟⡷⣯⣗⢯⣷⣣⢻⡾⣭⢷⣫⢧⡑⢧⣚⡵⡹⡞⣥⠻⣜⢧⣛⡼⢫⡜⣧⢻⡜⢧⡻⡜⣧⢓⡻⡜⣼⢡⡟⣾⡘⢮⢜⡣⢇⡞⡼⣈⠳⣭⠞⣥⣛⣮⢳⣭⠻⡜⢾⡼⣹⢏⡾⣍⢷⡹⣏⢾⡹⣞⡵⣫⢞⡵⢫⡽⢋⣾⣳⣞⢾⣱⢯⡞⣵⢫⡞⣥⢻⡜⢦⡳⢭⡜⢲⡿
+⣟⠽⣷⣫⠇⣞⢧⡞⣽⢺⡵⣛⣮⢟⡼⣯⡽⣯⣟⡷⣯⣟⡷⣯⣟⣷⣻⣶⢫⣗⢯⣻⣽⣳⢯⠾⣝⡎⠳⣼⢱⡏⣖⠻⡜⣎⢶⡙⣧⢻⡴⣋⢾⡹⢧⡝⣲⢫⠵⡹⢆⠣⣜⢣⡜⢣⢮⢱⣃⢮⡱⢎⡱⢌⠻⣖⡿⣼⢳⢮⣟⠼⣓⠾⣱⢏⡾⣙⢮⠵⣏⢾⡱⣎⠷⣙⢮⡝⣫⡴⣟⣾⣳⣞⣯⠷⣏⡾⣭⢷⡹⣎⢷⡚⡧⡝⣦⠹⣌⠷
+⣏⢯⡽⣿⣘⢮⡳⣝⢮⣳⠽⣝⣮⠿⣽⣳⣟⡷⣯⣟⣷⣯⢿⣷⣻⣾⣽⣞⣯⡞⡗⣮⠷⣯⣟⣻⢧⣏⡗⡤⣋⠼⣎⢳⡹⣜⢲⡙⡶⣻⢶⡹⢎⣝⢣⠞⣥⠋⣎⢱⡊⡵⢊⡖⣩⢓⢎⡆⢳⢢⡝⢬⡱⢎⠖⣸⣿⣯⣿⣻⣮⠹⡼⣩⠗⣎⡳⡝⣮⢳⡜⢣⣟⡼⣣⠟⣬⣞⣵⣫⣿⣳⣟⣾⣽⣻⡽⣯⢷⣫⠷⣝⡮⣽⡱⣛⡴⣛⠼⣆
+⣻⢎⡷⣃⢯⡞⣵⢫⡾⣱⠿⣽⣺⢟⣷⣻⢾⣽⣳⡿⣾⣽⡿⣞⣿⢾⣳⡿⣯⢿⡽⣎⣻⢳⣯⡽⣞⣮⡝⡶⣩⠞⡹⢆⡱⢌⢢⡙⢲⢹⠧⡙⠎⡜⠤⢣⡔⣩⢆⡳⢜⡡⢏⡜⡱⢎⢲⡽⣎⠶⢮⡱⣘⢎⡝⣆⠻⢿⡻⠟⢢⢛⡴⢋⡞⣥⢳⣙⣾⢣⠝⠣⠜⢳⣟⣾⡟⣞⣶⢿⣳⣯⣿⣞⣷⣯⢿⡽⣯⢯⣿⣹⢞⡵⣫⢵⡳⡭⣞⡹
+⢯⡞⣵⣸⢳⡞⣵⣫⢗⣯⢟⣳⢯⣟⡾⣽⣻⣞⣯⣿⣟⣷⣿⣿⣻⣿⣟⣿⣿⣻⣟⣾⡱⣏⡾⣽⣳⢧⡟⣵⢣⢏⡵⢋⡖⢣⢆⢧⠣⡜⢢⡉⢼⡓⣎⠳⣜⡱⢎⡵⢪⡕⣫⠜⡵⢫⡞⡽⣸⡙⡮⣕⢫⠞⡼⣌⡛⢦⠳⣍⠧⢦⢌⡱⠌⡔⢣⠜⡎⡔⢊⡱⣎⢷⣿⢳⡽⣾⣟⣿⡿⣽⣾⢿⣽⣾⣿⣻⣽⣻⢾⣭⣟⡾⣵⣫⢷⡹⣖⡹
+⣻⢼⢣⡳⣝⢾⡱⢯⡾⣭⢿⣽⣻⢾⣽⣻⡷⣟⣯⣷⣿⢿⣷⡿⣿⣽⡿⣿⣾⢿⣽⡷⣿⣭⣳⣏⠷⣯⣛⣮⢳⣎⡜⣣⢞⡱⢎⠮⡱⣌⠧⡜⢦⡹⣌⢳⣌⠳⣍⢞⡱⢎⡵⢫⠵⣫⢳⡱⢣⣝⠲⡝⢮⣙⠶⣡⢛⡬⣓⢎⡟⣬⢚⡴⢋⡜⣆⠩⢔⢢⢏⡵⣽⢟⣼⣳⣿⣟⣿⣽⣿⣟⣿⣿⢿⣷⣿⣻⣯⡿⣟⣾⡽⣞⣷⣹⣎⠷⣭⢳
+⣧⢻⢸⡳⣝⢮⡽⣳⣏⣯⣟⡾⣽⣻⢾⡷⣿⣿⣻⣯⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⡿⣽⡿⣞⣷⣞⠿⣧⣻⢼⡳⢮⡜⡱⢎⡵⣊⠷⣑⠮⣱⡙⢦⠳⣌⡳⣌⠗⣎⠮⣕⢫⣜⢣⡛⡴⢣⡝⣣⠮⣝⡹⢦⡙⢮⡱⣋⠶⣩⢎⡷⣌⠳⣜⢣⠞⡬⠹⣌⡷⣫⡽⣣⣿⢾⣻⣽⣾⢿⣻⣾⣿⣿⣿⣿⣿⣷⣿⡿⣽⣿⡷⣿⡽⣞⡷⣞⣻⣜⣳
+⣞⠭⣏⢷⡹⣎⡷⣻⣼⢳⣯⣟⡷⣿⣻⣟⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⡿⣿⡿⣽⣟⡾⣽⣻⢧⡟⣮⢽⣣⢏⡵⣋⠶⣩⠞⣌⢧⡱⢎⢣⡛⠴⣓⢬⡛⣬⢓⣎⠳⣬⢣⡝⡖⣭⢚⡥⣛⠴⣙⢦⣋⢧⢓⡭⣚⢥⣺⠵⣌⠳⣌⢣⠞⣰⣛⡬⢷⣭⢷⡿⣽⣻⣯⣿⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⢯⣿⣽⣻⡽⣖⢯⢶
+⡞⣝⢯⡞⡵⣏⣾⢳⣭⣟⡾⣽⣻⢷⡿⣯⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⣟⣾⣟⡷⣯⢟⣾⡹⣎⢷⣋⠶⣩⠞⣥⢛⡬⢲⢱⡊⣕⢪⠓⡭⢲⡙⡴⣋⡜⡳⣌⠳⣜⣣⠜⣣⠞⣡⢛⡜⣆⡓⣎⢣⠞⣡⠞⣜⠣⣌⠳⣌⠳⢎⡵⣣⢟⡽⣮⢿⣽⣳⣟⣷⢿⣻⣯⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣻⣞⣷⣻⡽⣞⢯
+⣽⢸⢧⡻⣝⠾⣼⣛⣮⢿⣽⣳⢿⣻⣿⢿⣽⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⢿⣾⣻⢾⡽⣯⢟⣶⢻⡝⣮⢭⡳⢥⡛⡴⣋⡜⣣⠇⡞⢤⢣⡙⣌⠓⠬⠱⠜⡲⣑⢎⠳⡌⣷⡩⢆⡝⣢⢃⡞⣤⠳⡜⠦⢛⢠⠻⣝⠲⣌⠳⣌⢻⣜⡳⣝⢾⡹⣧⣟⢾⣳⣟⡾⣿⣻⣽⣿⣻⣽⣿⣿⣿⣿⣿⣿⣿⣿⣯⣷⣿⢿⣽⣾⣳⢿⣹⣞
+⣞⢸⢧⡻⣜⣻⡵⣻⣞⢿⡾⣽⣻⣯⣿⢿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⢯⣟⣯⢿⣝⣻⡼⣳⢻⡜⣧⣛⢦⡙⢶⡱⢎⡕⡫⢜⠣⠖⡉⣄⠚⠬⣑⠲⡐⠤⡊⢍⡩⡙⡍⣋⠜⡩⢍⡩⠔⠣⠜⣐⠣⢢⠱⢠⠒⡌⠱⢎⡳⢎⡷⣹⢎⡷⣳⢞⣯⢷⣯⢿⡽⣟⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣻⡾⣽⢯⡷⣞
+--]=======================================================================]
+
+local Yield = task.wait
+local plr = game:GetService("Players").LocalPlayer
+local repStorage = game:GetService("ReplicatedStorage")
+local env = getgenv()
+
+env.AutoDig = false
+env.AutoBuy = false
+env.collect = false
+env.stealfromall = false
+
+local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "zurai02",
+   LoadingTitle = "Loading Script...",
+   LoadingSubtitle = "by zurai02",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = "BrainrotPolice",
+      FileName = "DumpConfig"
+   }
+})
+
+local MainTab = Window:CreateTab("Main", 4483362458)
+local PlayerTab = Window:CreateTab("Player", 4483362458)
+
+MainTab:CreateSection("Dump / Dig Options")
+
+MainTab:CreateToggle({
+   Name = "Steal from All",
+   CurrentValue = false,
+   Flag = "StealFromAllToggle",
+   Callback = function(v)
+      env.stealfromall = v
+      if not env.stealfromall then return end
+
+      task.spawn(function()
+         while env.stealfromall do
+            pcall(function()
+               local activePlots = workspace:FindFirstChild("ActivePlots")
+               if activePlots then
+                  for _, plot in pairs(activePlots:GetChildren()) do
+                     if not env.stealfromall then break end
+
+                     if plot.Name ~= "Plot" and plot.Name ~= tostring(plr.UserId) then
+                        local network = repStorage:FindFirstChild("Network")
+                        local remoteEvents = network and network:FindFirstChild("RemoteEvents")
+                        local lockpickEvent = remoteEvents and remoteEvents:FindFirstChild("LockpickGateOpen")
+
+                        if lockpickEvent then
+                           lockpickEvent:FireServer(plot, 30)
+                        end
+
+                        Yield()
+
+                        local placedItems = plot:FindFirstChild("PlacedItems")
+                        if placedItems then
+                           for _, item in pairs(placedItems:GetChildren()) do
+                              if not env.stealfromall then break end
+
+                              local primaryPart = item.PrimaryPart or item:FindFirstChild("PrimaryPart")
+                              if primaryPart and plr.Character then
+                                 plr.Character:MoveTo(primaryPart.Position)
+                                 Yield(0.5)
+
+                                 local attachment = primaryPart:FindFirstChild("Attachment")
+                                 local prompt = attachment and attachment:FindFirstChildOfClass("ProximityPrompt")
+                                 if prompt and typeof(fireproximityprompt) == "function" then
+                                    fireproximityprompt(prompt)
+                                 end
+
+                                 Yield(0.5)
+
+                                 local myPlot = activePlots:FindFirstChild(tostring(plr.UserId))
+                                 local tpPoint = myPlot and myPlot:FindFirstChild("TeleportPoint")
+                                 if tpPoint and plr.Character then
+                                    plr.Character:MoveTo(tpPoint.Position)
+                                 end
+
+                                 Yield(0.5)
+                              end
+                           end
+                        end
+                     end
+
+                     Yield()
+                  end
+               end
+            end)
+            Yield(1)
+         end
+      end)
+   end,
+})
+
+MainTab:CreateToggle({
+   Name = "Auto Dig",
+   CurrentValue = false,
+   Flag = "AutoDigToggle",
+   Callback = function(v)
+      env.AutoDig = v
+      if not env.AutoDig then return end
+
+      task.spawn(function()
+         while env.AutoDig do
+            pcall(function()
+               local network = repStorage:FindFirstChild("Network")
+               local remoteFunctions = network and network:FindFirstChild("RemoteFunctions")
+               local remoteEvents = network and network:FindFirstChild("RemoteEvents")
+
+               if remoteFunctions then
+                  local startDig = remoteFunctions:FindFirstChild("StartDigging")
+                  if startDig then
+                     startDig:InvokeServer()
+                  end
+
+                  Yield(1)
+
+                  local getSelectedItem = remoteFunctions:FindFirstChild("GetSelectedItem")
+                  if getSelectedItem then
+                     getSelectedItem:InvokeServer(2)
+                  end
+               end
+
+               if remoteEvents then
+                  local hashedEvent = remoteEvents:FindFirstChild("0a1baf564dbb5375")
+                  if hashedEvent then
+                     hashedEvent:FireServer(-1)
+                     hashedEvent:FireServer(0)
+                  end
+
+                  Yield(2)
+
+                  local endDig = remoteEvents:FindFirstChild("EndDigging")
+                  if endDig then
+                     endDig:FireServer("Succeeded", 3)
+                  end
+               end
+            end)
+            Yield(0.2)
+         end
+      end)
+   end,
+})
+
+MainTab:CreateToggle({
+   Name = "Auto Buy Best Shovel",
+   CurrentValue = false,
+   Flag = "AutoBuyToggle",
+   Callback = function(v)
+      env.AutoBuy = v
+      if not env.AutoBuy then return end
+
+      task.spawn(function()
+         local RarityOrder = {"Mythic", "Legendary", "Epic", "Rare", "Uncommon", "Common"}
+
+         local function getBestShovel()
+            local sharedSource = repStorage:FindFirstChild("SharedSource")
+            local gameData = sharedSource and sharedSource:FindFirstChild("GameData")
+            local shovelsModule = gameData and gameData:FindFirstChild("Shovels")
+
+            if not shovelsModule then return nil end
+
+            local shovelData = require(shovelsModule)
+            local leaderstats = plr:FindFirstChild("leaderstats")
+            local doubloons = leaderstats and leaderstats:FindFirstChild("Doubloons")
+            if not doubloons then return nil end
+
+            local money = doubloons.Value
+            local best = nil
+
+            for _, rarity in ipairs(RarityOrder) do
+               if shovelData.GetShovelsByRarity then
+                  local shovels = shovelData:GetShovelsByRarity(rarity)
+                  for _, shovel in ipairs(shovels) do
+                     if shovel.BaseCost and shovel.BaseCost <= money then
+                        if not best or shovel.BaseCost > best.BaseCost then
+                           best = shovel
+                        end
+                     end
+                  end
+                  if best then break end
+               end
+            end
+
+            return best
+         end
+
+         while env.AutoBuy do
+            pcall(function()
+               local best = getBestShovel()
+               if best then
+                  local network = repStorage:FindFirstChild("Network")
+                  local remoteEvents = network and network:FindFirstChild("RemoteEvents")
+                  local buyEvent = remoteEvents and remoteEvents:FindFirstChild("5844c2fc64759f91")
+
+                  if buyEvent then
+                     buyEvent:FireServer({
+                        ItemType = "Shovel",
+                        Name = best.Name
+                     })
+                  end
+               end
+            end)
+            Yield(5)
+         end
+      end)
+   end,
+})
+
+MainTab:CreateToggle({
+   Name = "Auto Collect Plot Money",
+   CurrentValue = false,
+   Flag = "AutoCollectToggle",
+   Callback = function(v)
+      env.collect = v
+      if not env.collect then return end
+
+      task.spawn(function()
+         while env.collect do
+            pcall(function()
+               local network = repStorage:FindFirstChild("Network")
+               local remoteEvents = network and network:FindFirstChild("RemoteEvents")
+               local collectEvent = remoteEvents and remoteEvents:FindFirstChild("CollectSavedPlotMoney")
+
+               if collectEvent then
+                  collectEvent:FireServer()
+               end
+            end)
+            Yield(1)
+         end
+      end)
+   end,
+})
+
+PlayerTab:CreateSection("Player Tweaks")
+
+PlayerTab:CreateSlider({
+   Name = "WalkSpeed",
+   Range = {16, 250},
+   Increment = 1,
+   Suffix = "Speed",
+   CurrentValue = 16,
+   Flag = "SpeedSlider",
+   Callback = function(val)
+      if plr.Character and plr.Character:FindFirstChildOfClass("Humanoid") then
+         plr.Character:FindFirstChildOfClass("Humanoid").WalkSpeed = val
+      end
+   end,
+})
+
+PlayerTab:CreateSlider({
+   Name = "JumpPower",
+   Range = {50, 300},
+   Increment = 5,
+   Suffix = "Power",
+   CurrentValue = 50,
+   Flag = "JumpSlider",
+   Callback = function(val)
+      if plr.Character and plr.Character:FindFirstChildOfClass("Humanoid") then
+         local hum = plr.Character:FindFirstChildOfClass("Humanoid")
+         hum.UseJumpPower = true
+         hum.JumpPower = val
+      end
+   end,
+})
