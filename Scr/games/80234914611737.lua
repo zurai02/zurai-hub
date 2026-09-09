e39c2022e18444050452843e1e434bd5b60a0edf
zurai02 <297417184+zurai02@users.noreply.github.com>
2026-09-09 11:36:08 +0500

    Update 80234914611737.lua

--- Scr/games/80234914611737.lua
+++ Scr/games/80234914611737.lua
@@ -90,7 +90,7 @@ local Window = Rayfield:CreateWindow({
    LoadingSubtitle = "by zurai02",
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = "BrainrotPolice",
+      FolderName = "zurai02 hub",
       FileName = "JetpackForBrainrotsConfig"
    }
 })
